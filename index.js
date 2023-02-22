3da5292d6d69e7e0ad00f32813c46327972601a6
coderB <flack319@gmail.com>
2023-02-22 15:16:39 +0900

    update the CRUD functions

--- index.js
+++ index.js
@@ -4,35 +4,81 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const app = express()
 
+const ejs = require('ejs')
+
+
+
 
 app.use(bodyParser.urlencoded({ extended  : false }))
 app.use(express.static(path.join(__dirname, "./public")))
+app.use(express.json())
+app.set('view engine', 'ejs')
 
-app.get("/api/post/create", (req, res, next) =>{ 
-    const { subject, author, content } = req.query;
+require('dotenv').config()
 
-    console.log(subject, author, content);
+const { PORT, MONGO_URI } = process.env
 
-    res.status(200).send("create post")
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+mongoose
+    .connect(MONGO_URI, { useNewUrlParser : true, useUnifiedTopology : true })
+    .then(() => {
+        console.log('success')
+    })
+    .catch(e => console.error(e));
+
+
+const Post = require('./models/post.model');
+
+app.post("/api/post/create", (req, res, next) =>{ 
+    Post.create(req.body)
+        .then(post => res.status(200).render('index'))
+        .catch(e => res.status(500).send(e))
+    
 })
+app.get("/api/post/list", (req, res, next) => {
+    Post.find()
+        .then(posts => res.status(200).json(posts))
+        .catch(e => res.status(500).send(e))
+})
+app.get("/api/post/:postId", (req, res, next) => { 
+    Post.findOneByPostId(req.params.postId)
+        .then(post => res.status(200).render('post', { post }))
+        .catch(e => res.status(500).send(e))
+    
+})
+app.get("/api/post/update/:postId", (req, res, next) => {
+    Post.findOneByPostId(req.params.postId)
+        .then(post => res.status(200).render('update', { post }))
+        .catch(e => res.status(500).send(e))
+})
+app.post("/api/post/update", (req, res, next) => {
+    const { postId } = req.body; 
+    Post.updateByPostId(postId, req.body)
+        .then(post => res.status(200).redirect(`/api/post/${postId}`))
+        .catch(e => res.status(500).send(e))
+})
+app.delete("/api/post/:postId", (req, res, next) => {
+    Post.deleteByPostId(req.params.postId)
+        .then(() => res.status(200).render('index'))
+        .catch(e => res.status(500).send(e))
+})
+
+
 app.post("/sign/login", (req, res, next) => { 
     const { username, password } = req.body;
-    
-    console.log(username, password);
-    
     res.status(200).send("login");
 })
+
 app.use("/", (req, res, next) => { 
-    console.log("hi")
-    res.status(200).sendFile(__dirname + "/public/index.html")
+    res.status(200).render('index')
 })
 
 
-
-const port = 8000;
 
 const httpServer = createServer(app)
 
-httpServer.listen(port, () => {
-    console.log(`server start...${port}`)
+httpServer.listen(PORT, () => {
+    console.log(`server start...${PORT}`)
 })
---
+++ models/post.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const autoIncrement = require('mongoose-auto-increment')
+const connection = mongoose.createConnection(process.env.MONGO_URI)
+
+autoIncrement.initialize(connection)
+const postSchema = new mongoose.Schema({
+    subject : {
+        type : String,
+        required : true, 
+    },
+    author :  {
+        type : String,
+        required : true,
+    },
+    content : { 
+        type : String,
+        required : true
+    }
+}, {
+    timestamps : true
+})
+postSchema.plugin(autoIncrement.plugin, {model : 'Post', field : 'postId'})
+postSchema.statics.create = function(payload){
+    const post = new this(payload);
+
+    return post.save();
+}
+postSchema.statics.findAll = function() { 
+    return this.find({});
+}
+postSchema.statics.findOneByPostId = function(postId) {
+    return this.findOne({ postId })
+}
+postSchema.statics.updateByPostId = function(postId, payload) {
+    return this.findOneAndUpdate({ postId }, payload, { new : true })
+}
+postSchema.statics.deleteByPostId = function(postId) { 
+    return this.remove({ postId })
+}
+module.exports = mongoose.model("Post", postSchema)
